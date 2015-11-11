af201cdc61930e71f2c5b45271153dde0228c93f
Nan Zhao <nanz@microsoft.com>
2015-11-10 23:10:25 -0800

    add more to document db library

--- DocumentDB/DocumentDB/DBClientFactory.cs
+++ DocumentDB/DocumentDB/DBClientFactory.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Azure.Documents;
 
 namespace DocumentDB
 {
-    public class Class1
+    public class DBClientFactory
     {
-       
     }
 }
---
+++ DocumentDB/DocumentDB/DBConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Configuration;
+
+namespace DocumentDB
+{
+    public interface IConfig
+    {
+        string ConnectionString(string name);
+        T Value<T>(string key, T defaultValue = default(T));
+    }
+
+
+    public class AppSettingsConfig : IConfig
+    {
+        public string ConnectionString(string name)
+        {
+            if (ConfigurationManager.ConnectionStrings[name] == null)
+            {
+                throw new ConfigurationErrorsException();
+            }
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        public T Value<T>(string key, T defaultValue = default(T))
+        {
+            var value = ConfigurationManager.AppSettings[key];
+            return value == null ? defaultValue : (T) Convert.ChangeType(value, typeof (T));
+                //Conversion.ConvertTo<T>(value);
+        }
+    }
+
+    public class DBConfig
+    {
+        public DBConfig(IConfig config)
+        {
+            Url = config.Value<string>("DBURL");
+            Key = config.Value<string>("privatekey");
+        }
+
+        public string Url { get; set; }
+        public string Key { get; set; }
+    }
+}
