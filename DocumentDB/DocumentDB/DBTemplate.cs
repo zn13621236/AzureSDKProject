8700d439d05611a4e7e7006373629b413c12002d
Nan Zhao <nanz@microsoft.com>
2015-11-11 15:17:20 -0800

    refactor db client

--- DocumentDB/DocumentDB/DBTemplate.cs
+++ DocumentDB/DocumentDB/DBTemplate.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
 
 namespace DocumentDB
 {
-    public class DBClientFactory
+    class DBTemplate
     {
-
-    
-
+     
     }
 }
